7d25331e8735ed9fc97d54e98bd23357a10c461a
Antoine Baldassari <baldassarian@gmail.com>
2019-02-22 14:31:54 -0500

    fix get_cepa

---
+++ R/CePa_v0.6/R/cepa_to_shiny.R
@@ -0,0 +1,13 @@
+
+get.cepa <- function(z_vec, pathway.name = NULL, pmap.path="extdata", type = "ora",  
+  thresh = 1.96, #required arguments
+  cen = "betweenness", 
+  cen.name = sapply(cen, function(x) ifelse(mode(x) == "name", deparse(x), x)), 
+  iter = 1000){
+  
+  if(type == "ora"){
+    cepa.ora.metab.all(names(z_vec[abs(z_vec) > thresh]), names(z_vec), pmap.path, pathway.name, cen, cen.name, iter)
+  }else{
+    cepa.univariate.metab.all(z_vec, pmap.path, pathway.name, cen, cen.name, iter)
+  }
+}

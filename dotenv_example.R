6b1d1eab7d04800ea06a132ba0ad15c4d3df4008
Gautham Venugopalan <gautham@gryphonscientific.com>
2019-02-22 11:48:55 -0500

    Update dotenv example

--- dotenv_example.R
+++ dotenv_example.R
@@ -1,3 +1,6 @@
+# example of how to use .env. The actual .env file is gitignored so you have to make your version from the .env.example template.
+# If you need to add more inputs, be sure to update the example file with placeholders.
+
 require(dotenv)
 dotenv::load_dot_env(file=".env") #change the file name from the default if you want to use a different file in a script
 
